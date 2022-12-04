3e53920bf5698ad7996f978684b04d834d181895
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2022-12-05 00:21:27 +0100

    wikipedia service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,13 @@ import { WikipediaService } from './wikipedia.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  pages = [];
+
   constructor(private wikipedia: WikipediaService) {}
 
   onTerm(term: string) {
-    const results = this.wikipedia.search(term);
-    window.alert(results);
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search;
+    });
   }
 }
